e74cd87c19f31a40a58953e1472bdcabdc6e6126
certifiedjo <josiahspringer9@gmail.com>
2024-03-04 23:20:11 -0500

    Add project files.

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sprijosiMVCAPP.Models;
+
+namespace sprijosiMVCAPP.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult jpView()
+        {
+            return View(uc.SprijosiTables.ToList());
+        }
+    }
+}
---
+++ Models/SprijosiTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sprijosiMVCAPP.Models;
+
+public partial class SprijosiTable
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserEmail { get; set; }
+}
